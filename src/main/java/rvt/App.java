0741a90d3ef5c9648ce01129dbcd2bf4ae9be460
24DP4AGobl <A240592AG@rvt.lv>
2025-09-25 11:52:08 +0300

    test file app recommit

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,25 @@
 package rvt;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Cik tev ir gadu?");
+        int age = Integer.valueOf(scanner.nextLine());
+
+        boolean isAdult = age >= 18;
+
+        if (age >= 18) {
+            System.out.println("Tu esi pilngadigs.");
+        }else{
+            System.out.println("Tu esi nepilngadigs, piekluves nav.");
+        }
+
+        if (isAdult){
+            System.out.println("Tu esi pilngadigs.");
+        }else{
+            System.out.println("Tu esi nepilngadigs, piekluves nav.");
+        } 
     }
 }
