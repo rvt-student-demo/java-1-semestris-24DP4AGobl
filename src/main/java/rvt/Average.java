ca8049fd6c4b4dd53c56fcd2379205aa4ccec4d1
24DP4AGobl <A240592AG@rvt.lv>
2025-10-09 11:03:02 +0300

    Commit of Average of three numbers

---
+++ src/main/java/rvt/Average.java
@@ -0,0 +1,21 @@
+package rvt;
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int num3 = Integer.valueOf(scanner.nextLine());
+
+        double averageOfTreeNum = (num1 + num2 + num3) / 3.0;
+
+        System.out.println(averageOfTreeNum);
+    }
+}
